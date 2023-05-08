e9f030fcb5a3e1e9ac84b28cb62975d01f7562a3
jennifer-brown <jenniferr.brown@berkeley.edu>
2023-05-08 19:48:54 +0000

    Structure updates

--- opioidtools/utils.py
+++ opioidtools/utils.py
@@ -8,9 +8,25 @@ import scipy.stats
 
 
 def to_numeric(df, col_name):
+    """converts column of dataframe to numeric
+    Parameters
+    ----------
+    df: dataframe
+        The dataframe containing the column to convert 
+    col_name: str
+        The column to convert to numeric
+    """
     return pd.to_numeric(df[col_name])
 
 def viz(df, col_name):
+    """creates pointplot of column for each state in dataframe
+    Parameters
+    ----------
+    df: dataframe
+        The dataframe with a column of states, 'State', and a numeric column
+    col_name: str
+        The numeric column to plot as the y-value
+        """
     fig, axs = plt.subplots(figsize=(8,45), nrows=17, ncols=3)
 
     # create state name array and reshape it
@@ -38,19 +54,31 @@ def viz(df, col_name):
     plt.show()
 
 def vis_avg_gdp_for_each_state(df):
+    """plots the average GDP from 2010-2014 for each state in dataframe
+    Parameters
+    ----------
+    df: dataframe
+        The dataframe containing the average GDP for each state
+        """
     # Sorted the data to make the graph clearer
     df = df.sort_values(ascending=False)
 
     # Plot the data
     plt.figure(figsize=(6,10))
     plt.barh(df.index, df.values)
-    plt.xlabel('Average GPD')
+    plt.xlabel('Average GDP')
     plt.ylabel('States')
     plt.title('Average GDP Value for Each State over 2010-2014')
     plt.savefig('figures/'+'Avg_GDP_for_Each_State_Over_Years')
     plt.show()
     
 def cal_print_min_median_max_gdp(df):
+    """returns the states with the minimum, median, and maximum GDP in & the values
+    Parameters
+    ----------
+    df: dataframe
+        The dataframe containing the average GDP for each state
+    """
     #Find the Minimum, Median, and Maximum avg GDP among states over 2010 to 2014
     median_gdp = df.median()
     median_gdp_states = df[df.eq(median_gdp)].index.tolist()
@@ -67,6 +95,16 @@ def cal_print_min_median_max_gdp(df):
     print(f"Maximum: {max_gdp:.2f} " f"State: {', '.join(max_gdp_states)}")
 
 def vis_two_avg_gdp_states(df, state1, state2):
+    """plots the average GDP of 2 states for 2010-2014
+    Parameters
+    ----------
+    df: dataframe
+        The dataframe containing the average GDP for the two states
+    state1: str
+        The first state to plot
+    state2: str
+        The second state to plot
+    """
     # Select the two states to plot plotting
     gdp_state1 = df[(df["State"] == state1)][["Year", "GDP_0101"]]
     gdp_state2 = df[(df["State"] == state2)][["Year", "GDP_0101"]]
@@ -87,6 +125,16 @@ def vis_two_avg_gdp_states(df, state1, state2):
     plt.show()
     
 def cal_print_t_test_CR(df, state1, state2):
+    """returns t-statistics for crude rate between 2 states and the p-value
+    Parameters
+    ----------
+    df: dataframe
+        The dataframe containing the crude rate for each state
+    state1: str
+        The first state to use in the t-test
+    state2: str
+        The second state to use in the t-test
+    """
     #T test for Crude Rate between two states
     t_stat, p_value = scipy.stats.ttest_ind(df[df['State']== state1]['Crude Rate'],
                      df[df['State']== state2]['Crude Rate'],
@@ -97,6 +145,12 @@ def cal_print_t_test_CR(df, state1, state2):
     
 
 def cal_print_t_test_CR_for_two_groups(df):
+    """returns t-statistics for crude rate between high GDP & low GDP state groups and its p-value
+    Parameters
+    ----------
+    df: dataframe
+        The dataframe containing the average GDP and crude rate for each state
+    """
     # Calculate the average GDP for each state over 2010 to 2014
     state_avg_gdp_2010_to_2014 = df.groupby('State')['GDP_0101'].mean()
     
